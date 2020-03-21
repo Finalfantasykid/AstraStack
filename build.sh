dd66ad6222c670f8be0213cc52592143f7fd4c92
David Turner <finalfantasykid13@gmail.com>
2020-03-21 10:45:43 -0600

    -Added libxcb to the build

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Get location of libxcb.so.1 so it can be included
+LIBXCB=`ldconfig -p | grep libxcb.so.1 | grep x86-64 | sed 's/^.*=> //'`
+
 rm -fr dist
-pyinstaller AstraStack.py
+pyinstaller AstraStack.py --add-binary "$LIBXCB:."
 rm -fr dist/AstraStack/share/icons/
 rm -fr dist/AstraStack/share/themes/
 cp -r share/themes dist/AstraStack/share/
