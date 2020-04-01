a2bf8bb3aee47be540e981ab8ae28953a032f413
David Turner <finalfantasykid13@gmail.com>
2020-03-31 23:04:41 -0600

    -Added Inno Setup file

--- build.sh
+++ build.sh
@@ -6,27 +6,36 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 
     rm -fr dist
     pyinstaller --add-binary "$LIBXCB:." \
-                --add-data "share:share" \
                 AstraStack.py
-    #rm -fr dist/AstraStack/share/icons/
-    #rm -fr dist/AstraStack/share/themes/
-    #cp -r share/themes dist/AstraStack/share/
-    #cp -r share/icons dist/AstraStack/share/
+    rm -fr dist/AstraStack/share/icons/
+    rm -fr dist/AstraStack/share/themes/
+    cp -r share/themes dist/AstraStack/share/
+    cp -r share/icons dist/AstraStack/share/
     cp -r ui dist/
-    cp bin/astra-stack dist/
-    cp bin/astra-stack.desktop dist/
+    cp scripts/astra-stack dist/
+    cp scripts/astra-stack.desktop dist/
     cd dist
     tar cfz ../AstraStack.tar.gz *
 elif [[ "$OSTYPE" == "msys" ]]; then
     rm -fr dist
-    rm AstraStack.zip
     pyinstaller --windowed \
                 --icon "ui/logo.ico" \
                 --hidden-import "packaging.requirements" \
                 --hidden-import "pkg_resources.py2_warn" \
                 --add-binary "/mingw64/bin/opencv_videoio_ffmpeg420_64.dll:." \
+                --exclude-module "FixTk" \
+                --exclude-module "tcl" \
+                --exclude-module "tk" \
+                --exclude-module "_tkinter" \
+                --exclude-module "tkinter" \
+                --exclude-module "Tkinter" \
                 AstraStack.py
-    cp -r ui dist/
-    cd dist
-    zip -r ../AstraStack *
+    rm -fr dist/AstraStack/share/icons/
+    rm -fr dist/AstraStack/share/themes/
+    rm -fr dist/AstraStack/share/locale/
+    cp -r share/themes dist/AstraStack/share/
+    cp -r share/icons dist/AstraStack/share/
+    cp -r ui dist/AstraStack/
+    C:/'Program Files (x86)'/'Inno Setup 6'/ISCC.exe scripts/inno.iss
 fi
+
