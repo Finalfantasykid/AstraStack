b70e35828d6bd12683175291ec63d92a3db2994b
David Turner <finalfantasykid13@gmail.com>
2020-04-01 00:24:16 -0600

    -Fixed install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Installing..."
+mkdir -p /usr/lib/astrastack
 cp -r AstraStack/* /usr/lib/astrastack
 cp AstraStack/astrastack /usr/bin/astrastack
 cp AstraStack/astrastack.desktop /usr/share/applications/
