6b134fee0b3e49b92846fea74cbf8629fc8ed31f
David Turner <finalfantasykid13@gmail.com>
2020-10-11 20:57:04 -0600

    -Removed unused imports

--- AstraStack.py
+++ AstraStack.py
@@ -1,8 +1,3 @@
-from Video import Video
-from Align import Align
-from Stack import Stack
-from Globals import g
-from pystackreg import StackReg
 from UI import run
 from multiprocessing import freeze_support
 
